f0a937559a7b2ec12575309161b50bdd45376a04
Johan Chane <johanchanex@gmail.com>
2025-04-24 16:37:21 +0800

    fixup! Add dotfiles

--- .config/nvim/lua/plugins/file_mgr.lua
+++ .config/nvim/lua/plugins/file_mgr.lua
@@ -1,84 +1,37 @@
 return {
   {
     "JohanChane/fm.nvim",
-    --dir = '~/.config/nvim/lua/fm.nvim',
     config = function()
       require("fm").setup({
-        ui = {
-          default = "float",
-          float = {
-            border = "single", -- see ':h nvim_open_win'
-            float_hl = "Normal", -- see ':h winhl'
-            border_hl = "Normal",
-            blend = 0, -- see ':h winblend'
-            height = 0.9, -- Num from 0 - 1 for measurements
-            width = 0.9,
-            x = 0.5, -- X and Y Axis of Window
-            y = 0.4,
-          },
-          split = {
-            direction = "left", -- see `:h nvim_open_win()`
-            width = 24,
-            height = 16,
-          },
-        },
         tools = {
-          ranger = {
-            create_win_cmd_format = "ranger --choosefiles %{choose_file}",
-            suffix = "l",
-          },
-          joshuto = {
-            create_win_cmd_format = "joshuto --file-chooser --output-file %{choose_file}",
-            suffix = "l",
-          },
+          -- This configuration means: when pressing `o` in yazi, it will run the following command.
+          -- The command is `yazi --chooser-file <choose_file> '<entry>'`
+          -- `_choose_file` is internally used by fm.nvim (the name is fixed, not customizable)
           yazi = {
-            create_win_cmd_format = "yazi --chooser-file %{choose_file}",
+            create_win_cmd_format = "yazi --chooser-file %{_choose_file} '%{entry}'",
             suffix = "o",
           },
-          lazygit = {
-            create_win_cmd_format = "lazygit",
-            suffix = "e",
-          },
         },
-        debug = false,
       })
 
       local function get_path(modifier)
-        return vim.fn.fnameescape(vim.fn.expand(modifier))
+        local res = vim.fn.expand(modifier)
+        if res == "" then
+          res = vim.fn.getcwd()
+        end
+        return res
       end
 
+      ---- ## Use `yazi` tool
       vim.keymap.set("n", "<M-d>", function()
-        --require('fm').open_fm({ name = 'ranger', other_params = { '.' } })
-        --require("fm").open_fm({ name = "joshuto", other_params = { "." } })
-        require("fm").open_fm({ name = "yazi", other_params = { "." } })
+        -- Final command: `yazi --chooser-file %{_choose_file} 'vim.fn.getcwd()'`
+        require("fm").open_fm({ name = "yazi", cmd_params = { entry = vim.fn.getcwd() } })
       end, { noremap = true })
+
       vim.keymap.set("n", "<M-f>", function()
-        -- final cmd: yazi --chooser-file %{choose_file} get_path('%:p')
-        require("fm").open_fm({ name = "yazi", other_params = { get_path("%:p") } })
-        --require("fm").open_fm({ name = "joshuto", other_params = { get_path("%:p:h") } })
-
-        --[[
-        local function open_ranger()
-          local path = get_path('%:p')
-          local other_params = {}
-          if path == '' then
-            other_params = {'.'}
-          else
-            other_params = { '--selectfile', path, '.' }
-          end
-          require('fm').open_fm({name = 'ranger', other_params = other_params})
-        end
-        open_ranger()
-        --]]
+        -- Final command: `yazi --chooser-file %{_choose_file} 'get_path("%:p")'`
+        require("fm").open_fm({ name = "yazi", cmd_params = { entry = get_path("%:p") } })
       end, { noremap = true })
-
-      -- If you want to create a command for `ranger`
-      vim.api.nvim_create_user_command("Ranger", function(opts)
-        require("fm").open_fm({ name = "ranger", other_params = { opts.args, "." } })
-      end, { nargs = "?", complete = "dir", bang = true })
-      vim.api.nvim_create_user_command("Lazygit", function(opt)
-        require("fm").open_fm({ name = "lazygit", other_params = { "-w", get_path("%:p:h"), opt.args } })
-      end, { nargs = "?", bang = true })
     end,
   },
 }
--- .config/nvim/lua/plugins/wsnavigator.lua
+++ .config/nvim/lua/plugins/wsnavigator.lua
@@ -1,17 +1,22 @@
 return {
-	{
-		"JohanChane/wsnavigator.nvim",
-		config = function()
-			require("wsnavigator").setup({
-				max_len_of_buffers = 7, -- Do not set this value above `20`, (recommended: `7`).
-				cb_for_too_many_buffers = function() -- Callback function when buffer count exceeds `max_len_of_buffers`
-					require("fzf-lua").buffers() -- Use `fzf-lua` for buffer switching when too many buffers are open. Please config your buffer switcher.
-				end,
-			})
+  {
+    "JohanChane/wsnavigator.nvim",
+    config = function()
+      require("wsnavigator").setup({
+        split = {
+          direction = "left", -- left, right, above, below. see `:h nvim_open_win()`
+          width = 48,
+          height = 16,
+        },
+        max_len_of_buffers = 7, -- Do not set this value above `20`, (recommended: `7`).
+        cb_for_too_many_buffers = function() -- Callback function when buffer count exceeds `max_len_of_buffers`
+          require("fzf-lua").buffers() -- Use `fzf-lua` for buffer switching when too many buffers are open.
+        end, -- Please config your buffer switcher.
+      })
 
-			vim.keymap.set("n", "<LocalLeader>f", function()
-				require("wsnavigator").open_wsn()
-			end, { noremap = true })
-		end,
-	},
+      vim.keymap.set("n", "<LocalLeader>f", function()
+        require("wsnavigator").toggle_wsn()
+      end, { noremap = true })
+    end,
+  },
 }
