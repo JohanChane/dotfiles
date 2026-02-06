532e6e243ed3b1329230894940080eaf0ff86ae1
JoeChaneX <joe.chane>
2026-02-06 13:08:22 +0800

    Add cmake alias

---
+++ app_configs/zsh/configs/utils.zsh
@@ -0,0 +1,3 @@
+## cmake
+alias cmake-release='cmake .. -DCMAKE_EXPORT_COMPILE_COMMANDS=ON -DCMAKE_BUILD_TYPE=Release'
+alias cmake-debug='cmake .. -DCMAKE_EXPORT_COMPILE_COMMANDS=ON -DCMAKE_BUILD_TYPE=Debug'
