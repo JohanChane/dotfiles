b757856586f82964a43849316aea020be904d4f7
JoeChaneX <joe.chane>
2026-06-10 01:05:37 +0800

    Revert "Add install_pkg_clangd"
    This reverts commit c16def4ae165aaf70dbb60a45aaf00fbb0033641.

--- app_installs/common/install.sh
+++ app_installs/common/install.sh
@@ -45,25 +45,3 @@ install_pkg_antidote() {
 
   git clone --depth=1 https://github.com/mattmc3/antidote.git ${ZDOTDIR:-$HOME}/.antidote
 }
-
-install_pkg_clangd() {
-  local arch_str
-  if [ "$(df_detect_arch)" = "amd64" ]; then
-    arch_str="linux"
-  else
-    echo "ERROR: clangd only supports amd64 for prebuilt binary" >&2
-    return 1
-  fi
-
-  local latest=$(curl -fsSL "https://api.github.com/repos/clangd/clangd/releases/latest" \
-              | grep -oP '"tag_name":\s*"v?\K[^"]+')
-
-  local bin_url="https://github.com/clangd/clangd/releases/download/${latest}/clangd-${arch_str}-${latest}.zip"
-
-  local tmpdir
-  tmpdir=$(mktemp -d)
-  curl -fsSL "$bin_url" -o "${tmpdir}/clangd.zip"
-  sudo 7z x "${tmpdir}/clangd.zip" -o"/usr/local/" -y > /dev/null
-  sudo ln -sf "/usr/local/clangd_${latest}/bin/clangd" /usr/local/bin/clangd
-  rm -rf "$tmpdir"
-}
